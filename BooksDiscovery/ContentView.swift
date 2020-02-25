eac876ee489069e84637b95a21867336a16b3f28
Alexey <al.hitrov55@gmail.com>
2020-02-25 19:10:32 +0300

    first commit

---
+++ BooksDiscovery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BooksDiscovery
+//
+//  Created by Alexey on 25.02.2020.
+//  Copyright © 2020 GETMOBIT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
